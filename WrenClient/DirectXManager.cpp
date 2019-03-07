cd60eb0ea5437fb0f4be44224aa414626793cd9e
dkestell <drew.kestell@gmail.com>
2019-03-07 00:09:44 -0800

    WIP

--- WrenClient/DirectXManager.cpp
+++ WrenClient/DirectXManager.cpp
@@ -1,6 +1,6 @@
 #include <d3d11.h>
-#include <d2d1.h>
-#include <dwrite.h>
+#include <d2d1_3.h>
+#include <dwrite_3.h>
 #include "DirectXManager.h"
 #include <exception>
 #include "Windows.h"
@@ -56,8 +56,8 @@ void DirectXManager::Initialize(HWND hWnd)
     // Create a render-target view   
     device->CreateRenderTargetView(backBuffer, NULL, &renderTargetView);
 
-    // Release reference to back-buffer
-    backBuffer->Release();
+    //// Release reference to back-buffer
+    //backBuffer->Release();
 
     // Bind the view to output/merger stage
     immediateContext->OMSetRenderTargets(1, &renderTargetView, NULL);
@@ -73,10 +73,84 @@ void DirectXManager::Initialize(HWND hWnd)
     immediateContext->RSSetViewports(1, &vp);
 
     // DWriteFactory
-    IDWriteFactory* writeFactory;
-    hr = DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), &writeFactory);
+    IDWriteFactory2* writeFactory;
+    hr = DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), reinterpret_cast<IUnknown**>(&writeFactory));
     if (FAILED(hr))
         throw std::exception("Failed to initialize DirectWrite Factory.");
+
+    // Direct2DFactory
+    ID2D1Factory2* d2dFactory;
+    D2D1_FACTORY_OPTIONS options;
+    options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+    hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, _uuidof(ID2D1Factory), &options, reinterpret_cast<void**>(&d2dFactory));
+    if (FAILED(hr))
+        throw std::exception("Failed to create Direct2D Factory.");
+    
+    // Get the dxgi device
+    IDXGIDevice1* dxgiDevice;
+    hr = device->QueryInterface(__uuidof(IDXGIDevice1), reinterpret_cast<void**>(&dxgiDevice));
+    if (FAILED(hr))
+        throw std::exception("Failed to get DXGI Device.");
+
+    // Create D2D Device
+    ID2D1Device1* d2dDevice;
+    hr = d2dFactory->CreateDevice(dxgiDevice, &d2dDevice);
+    if (FAILED(hr))
+        throw std::exception("Failed to get Direct2D Device.");
+
+    // Create D2D Device Context
+    ID2D1DeviceContext1* d2dDeviceContext;
+    hr = d2dDevice->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &d2dDeviceContext);
+    if (FAILED(hr))
+        throw std::exception("Failed to get Direct2D Device Context.");
+
+    // Set D2D Render Target to back buffer
+    D2D1_BITMAP_PROPERTIES1 bp;
+    bp.pixelFormat.format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    bp.pixelFormat.alphaMode = D2D1_ALPHA_MODE_IGNORE;
+    bp.dpiX = 96.0f;
+    bp.dpiY = 96.0f;
+    bp.bitmapOptions = D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW;
+    bp.colorContext = nullptr;
+
+    // Get reference to DXGI back-buffer
+    IDXGISurface* dxgiBuffer;
+    hr = swapChain->GetBuffer(0, __uuidof(IDXGISurface), reinterpret_cast<void**>(&dxgiBuffer));
+    if (FAILED(hr))
+        throw std::exception("Failed to retrieve the back buffer.");
+
+    // create the bitmap
+    ID2D1Bitmap1* targetBitmap;
+    hr = d2dDeviceContext->CreateBitmapFromDxgiSurface(dxgiBuffer, &bp, &targetBitmap);
+    if (FAILED(hr))
+        throw std::exception("Failed to create the Direct2D bitmap from the DXGI surface.");
+
+    // set the newly created bitmap as render target
+    d2dDeviceContext->SetTarget(targetBitmap);
+
+    // create standard brushes
+    ID2D1SolidColorBrush* yellowBrush;
+    if (FAILED(d2dDeviceContext->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Yellow), &yellowBrush)))
+        throw std::exception("Critical error: Unable to create the yellow brush!");
+
+    ID2D1SolidColorBrush* blackBrush;
+    if (FAILED(d2dDeviceContext->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Black), &blackBrush)))
+        throw std::exception("Critical error: Unable to create the black brush!");
+
+    ID2D1SolidColorBrush* whiteBrush;
+    if (FAILED(d2dDeviceContext->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &whiteBrush)))
+        throw std::exception("Critical error: Unable to create the white brush!");
+
+    // set up text formats
+
+    // FPS text
+    IDWriteTextFormat* textFormatFPS;
+    if (FAILED(writeFactory->CreateTextFormat(L"Lucida Console", nullptr, DWRITE_FONT_WEIGHT_LIGHT, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 12.0f, L"en-GB", &textFormatFPS)))
+        throw std::exception("Critical error: Unable to create text format for FPS information!");
+    if (FAILED(textFormatFPS->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING)))
+        throw std::exception("Critical error: Unable to set text alignment!");
+    if (FAILED(textFormatFPS->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_NEAR)))
+        throw std::exception("Critical error: Unable to set paragraph alignment!");
 }
 
 void DirectXManager::DrawScene()
@@ -86,4 +160,6 @@ void DirectXManager::DrawScene()
     HRESULT hr = swapChain->Present(0, 0);
     if (FAILED(hr))
         throw std::exception(FAILED_TO_SWAP_BUFFER);
+
+    // draw text
 }
